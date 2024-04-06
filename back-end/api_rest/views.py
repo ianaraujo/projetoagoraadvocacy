c92d28152f1dac8d58342ec03325b7815521803f
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2024-04-06 12:34:42 -0300

    :sparkles: feat: Adicionando a primeira api_view, getAllPropositions

--- back-end/api_rest/views.py
+++ back-end/api_rest/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Propositions
+from .serializers import PropositionSerializer
+
+
+@api_view(['GET'])
+def getAllPropositions():
+    try:
+        propositions= Propositions.objects.all()
+
+        serializer= PropositionSerializer(propositions, many=True)
+        return Response(serializer.data, status= status.HTTP_200_OK)
+    except:
+        return Response(status= status.HTTP_500_INTERNAL_SERVER_ERROR)
